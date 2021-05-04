bdd9ed8f496950aa8be18dbc74119ccc2ef2a7c1
Rishu Rana <bishtrahul150@gmail.com>
2021-05-04 14:37:00 +0530

    init commit

---
+++ src/first.java
@@ -0,0 +1,20 @@
+//Program to show Scanner class work
+
+import java.util.Scanner;
+
+public class first {
+    public static void main(String[] args) {
+
+        String name;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your name here: ");
+        String input = in.nextLine();
+
+        System.out.println(input);
+
+
+
+
+    }
+}
---
+++ src/second.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class second {
+    public static void main(String[] args) {
+     char c = '\u0000';
+
+        System.out.println(c);
+
+     
+    }
+}
